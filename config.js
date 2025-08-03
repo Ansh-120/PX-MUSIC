31cc1aa87d99867c2bc030f116dab5d3b98d8378
Ansh-120 <shbaraiya64@gmail.com>
2025-08-03 13:46:22 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,18 +1,16 @@
 
 
 module.exports = {
-  TOKEN: "",
+  TOKEN: "MTQwMDgzNDU4NTE1MDg4NjAwOA.GUASfk.j5ZthXvGcStS0UAx1crAdBkZdk66u_tmMW12jI",
   language: "en",
-  ownerID: ["1004206704994566164", ""], 
-  mongodbUri : "",
-  spotifyClientId : "",
-  spotifyClientSecret : "",
+  ownerID: ["1365298793662386190"], 
+  mongodbUri : "mongodb+srv://ANSH:<db_password>@cluster0.gex9ets.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
   setupFilePath: './commands/setup.json',
   commandsDir: './commands',  
   embedColor: "#1db954",
   activityName: "YouTube Music", 
   activityType: "LISTENING",  // Available activity types : LISTENING , PLAYING
-  SupportServer: "https://discord.gg/xQF9f9yUEM",
+  SupportServer: "https://discord.gg/QSdaRTdGhP",
   embedTimeout: 5, 
   errorLog: "", 
   nodes: [
